a7fda01f6118e09aa47be91dcd8e1a43cfb267d6
umarNurmuxamedov2013 <azizmaxmudov@gmail.com>
2024-04-18 08:51:45 +0500

    fist commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting!")
+let iD = prompt("ID ingizni kiriting")
+let passwrod = prompt("pasword ingizni kiriting!")
+
+const masrit = {
+    name :ism ,
+    spaceID : iD,
+    pas : passwrod
+}
+
+console.log(masrit);
